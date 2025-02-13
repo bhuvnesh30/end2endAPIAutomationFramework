5432e8311b198acce8c8fc8e4be9d69f0a5bc83c
Bhuvnesh Kumar <bhuvneshhh@gmail.com>
2025-02-13 17:42:30 +0530

    New changes

--- src/test/java/com/api/listeners/TestListener.java
+++ src/test/java/com/api/listeners/TestListener.java
@@ -7,38 +7,21 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
-	private static final Logger logger = LogManager.getLogger(TestListener.class);
+    private static final Logger logger = LogManager.getLogger(TestListener.class);
 
-	@Override
-	public void onStart(ITestContext context) {
-		logger.info("Test Suite started: " + context.getName());
-	}
+    @Override
+    public void onTestStart(ITestResult result) {
+        logger.info("Test Started: {}", result.getName());
+    }
 
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		logger.info("Test passed: " + result.getName());
-	}
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        logger.info("Test Passed: {}", result.getName());
+    }
 
-	@Override
-	public void onTestFailure(ITestResult result) {
-		logger.fatal("Test failed: " + result.getMethod().getMethodName());
-		logger.info("Description: " + result.getMethod().getDescription());
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult result) {
-		logger.info("Test skipped: " + result.getMethod().getMethodName());
-		logger.info("Description: " + result.getMethod().getDescription());
-	}
-
-	@Override
-	public void onFinish(ITestContext context) {
-		logger.info("Test Suite is ending: " + context.getName());
-	}
-
-	@Override
-	public void onTestStart(ITestResult result) {
-		logger.info("Test started: " + result.getMethod().getMethodName());
-		logger.info("Description: " + result.getMethod().getDescription());
-	}
+    @Override
+    public void onTestFailure(ITestResult result) {
+        logger.error("Test Failed: {}", result.getName());
+        logger.error("Exception: ", result.getThrowable());
+    }
 }
